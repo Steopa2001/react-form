64743304b41d124fb6bc26af180999e2744636ee
Steopa2001 <moraru495@gmail.com>
2025-08-31 12:12:41 +0200

    feat: Create an articles list

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,6 +1,20 @@
 import { useState } from "react"
 
+//Lista articoli 
+const articles = [
+  {
+    id: 1, title:'Primo Articolo'
+  },
+  {
+    id: 2, title:'Secondo Articolo'
+  },
+  {
+    id: 3, title:'Terzo Articolo'
+  }
+];
+
 function App() {
+
 
 }
 
